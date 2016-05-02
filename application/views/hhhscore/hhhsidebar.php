b6fbf8009ad0b4635a0ebb0de3a806b99642e59f
Ainul Hamdani <hamdani.ainul@gmail.com>
2016-05-02 18:30:16 +0800

    AH | Change sidebar structure

--- application/views/hhhscore/hhhsidebar.php
+++ application/views/hhhscore/hhhsidebar.php
@@ -1,38 +1,85 @@
 <div id="page" class="container">
     <div id="sidebar1">
-        <div id="bidan">
-            <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#bidanscore" style="width: 250px; text-align: left;">Bidan</button>
-            <div id="bidanscore" class="collapse">
-              <div id="headscore" style="padding-left:25px;">
-                  <a class="btn btn-link" href="<?php echo site_url() ."hhhscore/headscore"?>" style="width: 225px; text-align: left;">Head Score </a>
-              </div>
-              <div id="handscore" style="padding-left:25px;">
-                <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#antenatal" style="width: 225px; text-align: left;">Hand Score</button>
-                <div id="antenatal" class="collapse" style="padding-left:25px;">
-                    <div id="subantenatalQCI">
-                        <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#trimesterlist" style="width: 225px; text-align: left;">Pelayanan Antenatal (QCI)</button>
-                        <div id="trimesterlist" class="collapse" style="padding-left:40px;">
-                            <p><a href=<?php echo site_url() ."hhhscore/bidantrimester1"?>>Trimester 1</a></p>
-                            <p><a href=<?php echo site_url() ."hhhscore/bidantrimester2"?>>Trimester 2</a></p>
-                            <p><a href=<?php echo site_url() ."hhhscore/bidantrimester3"?>>Trimester 3</a></p>
+        <h2>Menu HHH SCORE</h2>
+        <div class="panel-group" id="accordion">
+            <div class="panel panel-default" id="bidans">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#bidan_menu">Bidan</a>
+                    </h4>
+                </div>
+                <div id="bidan_menu" class="panel-collapse collapse<?=($this->uri->segment(2)=='headscore'||(strpos($this->uri->segment(2),'trimester')!==false)||$this->uri->segment(2)=='heartscore'||$this->uri->segment(2)=='standar')?' in':''?>">
+                    <div class="panel panel-default">
+                        <div <?=($this->uri->segment(2)=='headscore')?' style="background-color:#909090"':''?> class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-parent="#bidan_menu" href="<?php echo site_url() ."hhhscore/headscore"?>">Head Score</a>
+                            </h4>
                         </div>
                     </div>
-                    <div id="subantenatalSC">
-                        <a class="btn btn-link" href="<?php echo site_url() ."hhhscore/standar"?>" style="width: 225px; text-align: left;">Cakupan (Standar & Non)</a>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#bidans" href="#handscore">Hand Score</a>
+                            </h4>
+                        </div>
+                        <div id="handscore" class="panel-collapse collapse<?=(strpos($this->uri->segment(2),'trimester')!==false||$this->uri->segment(2)=='standar')?' in':''?>">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a data-toggle="collapse" data-parent="#handscore" href="#qci">Pelayanan Antenatal (QCI)</a>
+                                    </h4>
+                                </div>
+                                <div id="qci" class="panel-collapse collapse<?=(strpos($this->uri->segment(2),'trimester')!==false)?' in':''?>">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading"<?=($this->uri->segment(2)=='bidantrimester1')?' style="background-color:#909090"':''?>>
+                                            <h4 class="panel-title">
+                                                <a data-parent="#qci" href="<?php echo site_url() ."hhhscore/bidantrimester1"?>">Trimester 1</a>
+                                            </h4>
+                                        </div>
+                                        <div class="panel-heading"<?=($this->uri->segment(2)=='bidantrimester2')?' style="background-color:#909090"':''?>>
+                                            <h4 class="panel-title">
+                                                <a data-parent="#qci" href="<?php echo site_url() ."hhhscore/bidantrimester2"?>">Trimester 2</a>
+                                            </h4>
+                                        </div>
+                                        <div class="panel-heading"<?=($this->uri->segment(2)=='bidantrimester3')?' style="background-color:#909090"':''?>>
+                                            <h4 class="panel-title">
+                                                <a data-parent="#qci" href="<?php echo site_url() ."hhhscore/bidantrimester3"?>">Trimester 3</a>
+                                            </h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading"<?=($this->uri->segment(2)=='standar')?' style="background-color:#909090"':''?>>
+                                    <h4 class="panel-title">
+                                        <a data-parent="#bidan_menu" href="<?php echo site_url() ."hhhscore/standar"?>">Cakupan Standar</a>
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div <?=($this->uri->segment(2)=='bidanbyform'&&$this->uri->segment(3)=='Sengkol')?' style="background-color:#909090"':''?> class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-parent="#bidan_menu" href="<?php echo site_url() ."hhhscore/heartscore"?>">Heart Score</a>
+                            </h4>
+                        </div>
                     </div>
                 </div>
-                
-              </div>
-              <div id="subbidanentrytanggal" style="padding-left:25px;">
-                  <a class="btn btn-link" href="<?php echo site_url() ."hhhscore/heartscore"?>" style="width: 225px; text-align: left;">Heart Score </a>
-              </div>
             </div>
-         </div>
-        <div id='gizi'>
-            <a class="btn btn-link" href="#" style="width: 225px; text-align: left;">Gizi</a>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Gizi</a>
+                    </h4>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Jurim</a>
+                    </h4>
+                </div>
+            </div>
         </div>
-        <div id='jurim'>
-            <a class="btn btn-link" href="#" style="width: 225px; text-align: left;">Jurim</a>
-        </div>
-        
     </div>
