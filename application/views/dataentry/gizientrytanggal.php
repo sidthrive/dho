144d963b58be64d3282b900d3868a12ec3a29bc0
Johan SID <johanw1076@gmail.com>
2016-08-16 13:53:21 +0800

    AH | add Gizi views for dataentry

---
+++ application/views/dataentry/gizientrytanggal.php
@@ -0,0 +1,78 @@
+<?php
+    if($mode=="Mingguan"){
+        $opt = "Minggu";
+    }elseif($mode=="Bulanan"){
+        $opt = "Bulan";
+    }else{
+        $opt = "Tanggal";
+    }
+    
+?>    
+    <div id="content">
+        <div id="text">
+            Tampilkan Berdasarkan : <select>
+                <option id="tgl" value=""<?=$opt=="Tanggal"?" selected":""?>>Tanggal</option>
+                <option id="mng" value="Mingguan"<?=$opt=="Minggu"?" selected":""?>>Minggu</option>
+                <option id="bln" value="Bulanan"<?=$opt=="Bulan"?" selected":""?>>Bulan</option>
+            </select>
+        </div>
+        <br>
+        <div id="text" style="text-align: center;">
+            <h3>Total Entri tiap <?=$opt?></h3>
+            <h3>Puskesmas <?=$kecamatan?></h3>
+        </div>
+        <div id="container">
+            <!--
+                graphic container
+            -->
+            <?php foreach($data as $user => $form){
+            ?>
+            <br>
+            <div title="Desa <?=ucwords($user)?>">
+                    <div id="">
+                        <center><span style="font-size:16px; font-family:'Droid Sans',Arial,Verdana,sans-serif;"><strong>Desa <?=ucwords($user)?></strong>
+                        <!-- START Script Block for Chart -->
+                        <div id="<?=str_replace(' ', '_', $user);?>" align="center">
+                        </div>
+                        <!-- END Script Block for Chart -->                
+                    </div>
+                </div>
+            <br><br>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
+
+<script src="<?=base_url()?>assets/js/highcharts.js"></script>
+<script src="<?=base_url()?>assets/js/exporting.js"></script>
+<script src="<?=base_url()?>assets/js/functions.js"></script>
+<script>
+    var json = <?=json_encode($data)?>;
+    <?php 
+    if(isset($mode)){
+        if($mode=='Mingguan'){
+            echo '$.fn.showChartDataEntryMinggu(json);';
+        }else{
+            echo '$.fn.showChartDataEntryBulan(json);';
+        }
+    }else{
+        echo '$.fn.showChartDataEntryTanggal(json);';
+    } ?> 
+    var mode = $( "select option:selected" ).attr("id");
+    $( "select" ).change(function() {
+        $( "select option:selected" ).each(function() {
+            var newmode = $( "select option:selected" ).attr("id");
+            if(mode!=newmode){
+                var modeurl = "";
+                if(newmode=="mng"){
+                    modeurl = "/Mingguan";
+                }else if(newmode=="bln"){
+                    modeurl = "/Bulanan";
+                }
+                window.location.href = "<?=base_url()."dataentry/gizibytanggal/".$kecamatan?>"+modeurl;
+            }
+            console.log($( "select option:selected" ).attr("id"));
+        });    
+    }).trigger( "change" );
+</script>
