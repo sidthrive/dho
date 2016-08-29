337771d936c58c50e487dfeac30e94a8532c25ba
AinulHamdani <hamdani.ainul@gmail.com>
2016-08-29 11:33:07 +0800

    AH | add status gizi functionality demo

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -221,10 +221,59 @@ class Laporan extends CI_Controller{
         $this->load->view("footer");
     }
     
-    public function statusGizi(){
+    public function cakupanGizi(){
+        $xlsForm = [];
+        $user   =  ['Lekor'=>0,'Saba'=>0,'Pendem'=>0,'Setuta'=>0,'Jango'=>0,'Janapria'=>0,'Ketara'=>0,'Sengkol'=>0,'Kawo'=>0,'Tanak Awu'=>0,'Pengembur'=>0,'Segala Anyar'=>0];
+        $user_village = ['user1'=>'Lekor','user2'=>'Saba','user3'=>'Pendem','user4'=>'Setuta','user5'=>'Jango','user6'=>'Janapria','user8'=>'Ketara','user9'=>'Sengkol','user10'=>'Sengkol','user11'=>'Kawo','user12'=>'Tanak Awu','user13'=>'Pengembur','user14'=>'Segala Anyar'];
+        $form = $user;
+        $series1['page']='ds';
+        $series1['form']=['Lekor'=>rand(50,90),'Saba'=>rand(50,90),'Pendem'=>rand(50,90),'Setuta'=>rand(50,90),'Jango'=>rand(50,90),'Janapria'=>rand(50,90),'Ketara'=>rand(50,90),'Sengkol'=>rand(50,90),'Kawo'=>rand(50,90),'Tanak Awu'=>rand(50,90),'Pengembur'=>rand(50,90),'Segala Anyar'=>rand(50,90)];
+        $series1['y_label']='persentase';
+        $series1['series_name']='persentase';
+        array_push($xlsForm, $series1);
+        
+        
+        $series1['page']='nd';
+        $series1['form']=['Lekor'=>rand(50,90),'Saba'=>rand(50,90),'Pendem'=>rand(50,90),'Setuta'=>rand(50,90),'Jango'=>rand(50,90),'Janapria'=>rand(50,90),'Ketara'=>rand(50,90),'Sengkol'=>rand(50,90),'Kawo'=>rand(50,90),'Tanak Awu'=>rand(50,90),'Pengembur'=>rand(50,90),'Segala Anyar'=>rand(50,90)];;
+        array_push($xlsForm, $series1);
+        
+        $series1['page']='bgmd';
+        $series1['form']=['Lekor'=>rand(50,90),'Saba'=>rand(50,90),'Pendem'=>rand(50,90),'Setuta'=>rand(50,90),'Jango'=>rand(50,90),'Janapria'=>rand(50,90),'Ketara'=>rand(50,90),'Sengkol'=>rand(50,90),'Kawo'=>rand(50,90),'Tanak Awu'=>rand(50,90),'Pengembur'=>rand(50,90),'Segala Anyar'=>rand(50,90)];;
+        array_push($xlsForm, $series1);
+        
+        $series1['page']='vitfe';
+        $series1['form']=['Lekor'=>rand(50,90),'Saba'=>rand(50,90),'Pendem'=>rand(50,90),'Setuta'=>rand(50,90),'Jango'=>rand(50,90),'Janapria'=>rand(50,90),'Ketara'=>rand(50,90),'Sengkol'=>rand(50,90),'Kawo'=>rand(50,90),'Tanak Awu'=>rand(50,90),'Pengembur'=>rand(50,90),'Segala Anyar'=>rand(50,90)];;
+        array_push($xlsForm, $series1);
+        
+        $series1['page']='anemia';
+        $series1['form']=['Lekor'=>rand(50,90),'Saba'=>rand(50,90),'Pendem'=>rand(50,90),'Setuta'=>rand(50,90),'Jango'=>rand(50,90),'Janapria'=>rand(50,90),'Ketara'=>rand(50,90),'Sengkol'=>rand(50,90),'Kawo'=>rand(50,90),'Tanak Awu'=>rand(50,90),'Pengembur'=>rand(50,90),'Segala Anyar'=>rand(50,90)];;
+        array_push($xlsForm, $series1);
+        
+        $series1['page']='kek';
+        $series1['form']=['Lekor'=>rand(50,90),'Saba'=>rand(50,90),'Pendem'=>rand(50,90),'Setuta'=>rand(50,90),'Jango'=>rand(50,90),'Janapria'=>rand(50,90),'Ketara'=>rand(50,90),'Sengkol'=>rand(50,90),'Kawo'=>rand(50,90),'Tanak Awu'=>rand(50,90),'Pengembur'=>rand(50,90),'Segala Anyar'=>rand(50,90)];;
+        array_push($xlsForm, $series1);
+        
+        $series1['page']='gibur';
+        $series1['form']=['Lekor'=>rand(50,90),'Saba'=>rand(50,90),'Pendem'=>rand(50,90),'Setuta'=>rand(50,90),'Jango'=>rand(50,90),'Janapria'=>rand(50,90),'Ketara'=>rand(50,90),'Sengkol'=>rand(50,90),'Kawo'=>rand(50,90),'Tanak Awu'=>rand(50,90),'Pengembur'=>rand(50,90),'Segala Anyar'=>rand(50,90)];;
+        array_push($xlsForm, $series1);
+        
+        $series1['page']='fe13';
+        $series1['form']=['Lekor'=>rand(50,90),'Saba'=>rand(50,90),'Pendem'=>rand(50,90),'Setuta'=>rand(50,90),'Jango'=>rand(50,90),'Janapria'=>rand(50,90),'Ketara'=>rand(50,90),'Sengkol'=>rand(50,90),'Kawo'=>rand(50,90),'Tanak Awu'=>rand(50,90),'Pengembur'=>rand(50,90),'Segala Anyar'=>rand(50,90)];;
+        array_push($xlsForm, $series1);
+        
+        $series1['page']='asi';
+        $series1['form']=['Lekor'=>rand(50,90),'Saba'=>rand(50,90),'Pendem'=>rand(50,90),'Setuta'=>rand(50,90),'Jango'=>rand(50,90),'Janapria'=>rand(50,90),'Ketara'=>rand(50,90),'Sengkol'=>rand(50,90),'Kawo'=>rand(50,90),'Tanak Awu'=>rand(50,90),'Pengembur'=>rand(50,90),'Segala Anyar'=>rand(50,90)];;
+        array_push($xlsForm, $series1);
+        
+        $series1['page']='bblr';
+        $series1['form']=['Lekor'=>rand(50,90),'Saba'=>rand(50,90),'Pendem'=>rand(50,90),'Setuta'=>rand(50,90),'Jango'=>rand(50,90),'Janapria'=>rand(50,90),'Ketara'=>rand(50,90),'Sengkol'=>rand(50,90),'Kawo'=>rand(50,90),'Tanak Awu'=>rand(50,90),'Pengembur'=>rand(50,90),'Segala Anyar'=>rand(50,90)];;
+        array_push($xlsForm, $series1);
+        
+        
+        $dataXLS['xlsForm']=$xlsForm;
         $this->load->view("header");
         $this->load->view("laporan/laporansidebar");
-        $this->load->view("laporan/statusgizi");
+        $this->load->view("laporan/statusgizi",$dataXLS, false);
         $this->load->view("footer");
     }
     
