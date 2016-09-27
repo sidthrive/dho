0014ceae1231329342c45806ff69b993e0f572b4
AinulHamdani <hamdani.ainul@gmail.com>
2016-09-27 11:37:01 +0800

    AH | move month selection to view

--- application/views/laporan/laporansidebar.php
+++ application/views/laporan/laporansidebar.php
@@ -11,74 +11,10 @@
                 <div id="bidan_menu" class="panel-collapse collapse<?=($this->uri->segment(2)=='cakupanindikatorpws'||$this->uri->segment(2)=='downloadbidanpws')?' in':''?>">
                     <div class="panel panel-default">
                         <div class="panel panel-default">
-                            <div class="panel-heading">
+                            <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws')?' style="background-color:#909090"':''?>>
                                 <h4 class="panel-title">
-                                    <a data-toggle="collapse" data-parent="#bidan_menu" href="#bidan_month">Cakupan Indikator PWS</a>
+                                    <a href="<?php echo site_url() ."laporan/cakupanindikatorpws"?>">Cakupan Indikator PWS</a>
                                 </h4>
-                            </div>
-                            <div id="bidan_month" class="panel-collapse collapse<?=($this->uri->segment(2)=='cakupanindikatorpws')?' in':''?>">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='januari')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/januari"?>">Januari</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='februari')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/februari"?>">Februari</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='maret')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/maret"?>">Maret</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='april')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/april"?>">April</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='mei')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/mei"?>">Mei</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='juni')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/juni"?>">Juni</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='juli')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/juli"?>">Juli</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='agustus')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/agustus"?>">Agustus</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='september')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/september"?>">September</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='oktober')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/oktober"?>">Oktober</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='november')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/november"?>">November</a>
-                                        </h4>
-                                    </div>
-                                    <div class="panel-heading" <?=($this->uri->segment(2)=='cakupanindikatorpws'&&$this->uri->segment(3)=='desember')?' style="background-color:#909090"':''?>>
-                                        <h4 class="panel-title">
-                                            <a href="<?php echo site_url() ."laporan/cakupanindikatorpws/desember"?>">Desember</a>
-                                        </h4>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                         <div class="panel-heading"<?=($this->uri->segment(2)=='downloadbidanpws')?' style="background-color:#909090"':''?>>
@@ -97,19 +33,10 @@
                 </div>
                 <div id="collapse2" class="panel-collapse collapse<?=($this->uri->segment(2)=='cakupangizi'||$this->uri->segment(2)=='downloadgizipws')?' in':''?>">
                     <div class="panel panel-default">
-                        <div class="panel-heading">
+                        <div class="panel-heading" <?=($this->uri->segment(2)=='cakupangizi')?' style="background-color:#909090"':''?>>
                             <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#collapse2" href="#gizi_cov">Cakupan Gizi</a>
+                                <a href="<?php echo site_url() ."laporan/cakupangizi"?>">Cakupan Gizi</a>
                             </h4>
-                        </div>
-                        <div id="gizi_cov" class="panel-collapse collapse<?=$this->uri->segment(2)=='cakupangizi'?' in':''?>">
-                            <div class="panel panel-default">
-                                <div class="panel-heading" <?=($this->uri->segment(2)=='cakupangizi')?' style="background-color:#909090"':''?>>
-                                    <h4 class="panel-title">
-                                        <a href="<?php echo site_url() ."laporan/cakupangizi"?>">Bulan ini</a>
-                                    </h4>
-                                </div>
-                            </div>
                         </div>
                     </div>
                     <div class="panel panel-default">
