8477d45e99a6a2c6be28e868e4b846679f404b67
AinulHamdani <hamdani.ainul@gmail.com>
2016-09-29 11:33:50 +0800

    AH | add user strict in sidebar

--- application/views/dataentry/fhw/dataentrysidebar.php
+++ application/views/dataentry/fhw/dataentrysidebar.php
@@ -2,6 +2,7 @@
     <div id="sidebar1">
         <h2>Menu Laporan</h2>
         <div class="panel-group" id="accordion">
+            <?php if($this->session->userdata('tipe')=="bidan"||$this->session->userdata('tipe')=="all"){ ?>
             <div class="panel panel-default" id="bidans">
                 <div class="panel-heading">
                     <h4 class="panel-title">
@@ -25,48 +26,56 @@
                     </div>
                 </div>
             </div>
-            <div class="panel panel-default">
+            <?php } ?>
+            <?php if($this->session->userdata('tipe')=="gizi"||$this->session->userdata('tipe')=="all"){ ?>
+            <div class="panel panel-default" id="gizis">
                 <div class="panel-heading">
                     <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Gizi</a>
+                        <a data-toggle="collapse" data-parent="#accordion" href="#gizi_menu">Gizi</a>
                     </h4>
                 </div>
-                <div id="collapse2" class="panel-collapse collapse<?=$this->uri->segment(2)=='gizi'?' in':''?>">
-                    <div class="panel panel-default">
-                        <div class="panel-heading"<?=($this->uri->segment(2)=='gizi'&&$this->uri->segment(3)=='Sengkol')?' style="background-color:#909090"':''?>>
+                <div id="gizi_menu" class="panel-collapse collapse<?=($this->uri->segment(2)=='gizibyform'||$this->uri->segment(2)=='gizibytanggal')?' in':''?>">
+                    <div class="panel panel-default" <?=($this->uri->segment(2)=='gizibyform')?' style="background-color:#909090"':''?>>
+                        <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a href="<?php echo site_url() ."dataentry/gizi/Sengkol"?>">Kecamatan Sengkol</a>
-                            </h4>
-                        </div>
-                        <div class="panel-heading"<?=($this->uri->segment(2)=='gizi'&&$this->uri->segment(3)=='Janapria')?' style="background-color:#909090"':''?>>
-                            <h4 class="panel-title">
-                                <a href="<?php echo site_url() ."dataentry/gizi/Janapria"?>">Kecamatan Janapria</a>
+                                <a data-parent="#gizis" href="<?php echo site_url() ."dataentry/gizibyform"?>">Total Entry Tiap From</a>
                             </h4>
                         </div>
                     </div>
-                </div>
-                    
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Jurim</a>
-                    </h4>
-                </div>
-                <div id="collapse3" class="panel-collapse collapse<?=$this->uri->segment(2)=='jurim'?' in':''?>">
-                    <div class="panel panel-default">
-                        <div class="panel-heading"<?=($this->uri->segment(2)=='jurim'&&$this->uri->segment(3)=='Sengkol')?' style="background-color:#909090"':''?>>
+                    <div class="panel panel-default" <?=($this->uri->segment(2)=='gizibytanggal')?' style="background-color:#909090"':''?>>
+                        <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a href="<?php echo site_url() ."dataentry/Jurim/Sengkol"?>">Kecamatan Sengkol</a>
-                            </h4>
-                        </div>
-                        <div class="panel-heading"<?=($this->uri->segment(2)=='jurim'&&$this->uri->segment(3)=='Janapria')?' style="background-color:#909090"':''?>>
-                            <h4 class="panel-title">
-                                <a href="<?php echo site_url() ."dataentry/Jurim/Janapria"?>">Kecamatan Janapria</a>
+                                <a data-parent="#gizis" href="<?php echo site_url() ."dataentry/gizibytanggal"?>">Total Entry Tiap Tanggal</a>
                             </h4>
                         </div>
                     </div>
                 </div>
             </div>
+            <?php } ?>
+            <?php if($this->session->userdata('tipe')=="vaksinator"||$this->session->userdata('tipe')=="all"){ ?>
+            <div class="panel panel-default" id="vaksinators">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#vak_menu">Vaksinator</a>
+                    </h4>
+                </div>
+                <div id="vak_menu" class="panel-collapse collapse<?=($this->uri->segment(2)=='vaksinatorbyform'||$this->uri->segment(2)=='vaksinatorbytanggal')?' in':''?>">
+                    <div class="panel panel-default" <?=($this->uri->segment(2)=='vaksinatorbyform')?' style="background-color:#909090"':''?>>
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-parent="#vaksinators" href="<?php echo site_url() ."dataentry/vaksinatorbyform"?>">Total Entry Tiap From</a>
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="panel panel-default" <?=($this->uri->segment(2)=='vaksinatorbytanggal')?' style="background-color:#909090"':''?>>
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-parent="#vaksinators" href="<?php echo site_url() ."dataentry/vaksinatorbytanggal"?>">Total Entry Tiap Tanggal</a>
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
         </div>
     </div>
