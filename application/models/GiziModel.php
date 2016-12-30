4d9769266984f77f30be170fa147a4966ee009fa
AinulHamdani <hamdani.ainul@gmail.com>
2016-12-30 17:06:34 +0800

    AH | add view by visit date function to Model

--- application/models/GiziModel.php
+++ application/models/GiziModel.php
@@ -100,6 +100,89 @@ class GiziModel extends CI_Model{
         return $result_data;
     }
     
+    public function getCountPerDayByVisitDate($kecamatan="",$mode="",$range=""){
+        if($mode!=""){
+            return self::getCountPerMode($kecamatan,$mode);
+        }
+        date_default_timezone_set("Asia/Makassar"); 
+        $giziDB = $this->load->database('gizi', TRUE);
+        $query  = $giziDB->query("SHOW TABLES FROM opensrp_gizi");
+        $table_default = [
+            'kunjungan_gizi'=>'Kunjungan Gizi'];
+        //retrieve the tables name
+        $tables = array();
+        foreach ($query->result() as $table){
+            if(array_key_exists($table->Tables_in_opensrp_gizi, $table_default)){
+                $tables[$table->Tables_in_opensrp_gizi]=$table_default[$table->Tables_in_opensrp_gizi];
+            }
+        }
+        
+       if($kecamatan=='Sengkol'){
+            $users = ['gizi8'=>'Ketara','gizi9'=>'Sengkol','gizi10'=>'Sengkol','gizi11'=>'Kawo','gizi12'=>'Tanak Awu','gizi13'=>'Pengembur','gizi14'=>'Segala Anyar'];
+        }elseif($kecamatan=='Janapria'){
+            $users = ['gizi1'=>'Lekor','gizi2'=>'Saba','gizi3'=>'Pendem','gizi4'=>'Setuta','gizi5'=>'Jango','gizi6'=>'Janapria'];
+        }else{
+            return;
+        }
+        
+        //make result array from the tables name
+        if($range!=""){
+            foreach ($users as $user=>$desa){
+                $begin = new DateTime($range[0]);
+                $end = new DateTime($range[1]);
+                $data = array();
+                for($i=$begin;$begin<=$end;$i->modify('+1 day')){
+                    $date    = $i->format("Y-m-d");
+                    $data[$date] = 0;
+                }
+                $result_data[$desa] = $data;
+            }
+        }else{
+            foreach ($users as $user=>$desa){
+                $data = array();
+                for($i=1;$i<=30;$i++){
+                    $day     = 30-$i;
+                    $date    = date("Y-m-d",  strtotime("-".$day." days"));
+                    $data[$date] = 0;
+                }
+                $result_data[$desa] = $data;
+            }
+        }
+        
+        
+        foreach ($tables as $table=>$legend){
+            //query tha data
+            if($range!=""){
+                if($kecamatan=='Sengkol'){
+                    $query = $giziDB->query("SELECT userid, tanggalPenimbangan as submissiondate,count(*) as counts from ".$table." where (userid='gizi8' or userid='gizi9' or userid='gizi10' or userid='gizi11' or userid='gizi12' or userid='gizi13' or userid='gizi14') and (tanggalPenimbangan >= '".$range[0]."' and tanggalPenimbangan <= '".$range[1]."') group by userid, tanggalPenimbangan");
+                }
+                elseif($kecamatan=='Janapria'){
+                    $query = $giziDB->query("SELECT userid, tanggalPenimbangan as submissiondate,count(*) as counts from ".$table." where (userid='gizi1' or userid='gizi2' or userid='gizi3' or userid='gizi4' or userid='gizi5' or userid='gizi6') and (tanggalPenimbangan >= '".$range[0]."' and tanggalPenimbangan <= '".$range[1]."') group by userid, tanggalPenimbangan");
+                }
+            }else{
+                if($kecamatan=='Sengkol'){
+                    $query = $giziDB->query("SELECT userid, tanggalPenimbangan as submissiondate,count(*) as counts from ".$table." where (userid='gizi8' or userid='gizi9' or userid='gizi10' or userid='gizi11' or userid='gizi12' or userid='gizi13' or userid='gizi14') and (tanggalPenimbangan >= '".date("Y-m-d",strtotime("-30 days"))."' and tanggalPenimbangan <= '".date("Y-m-d")."') group by userid, tanggalPenimbangan");
+                }
+                elseif($kecamatan=='Janapria'){
+                    $query = $giziDB->query("SELECT userid, tanggalPenimbangan as submissiondate,count(*) as counts from ".$table." where (userid='gizi1' or userid='gizi2' or userid='gizi3' or userid='gizi4' or userid='gizi5' or userid='gizi6') and (tanggalPenimbangan >= '".date("Y-m-d",strtotime("-30 days"))."' and tanggalPenimbangan <= '".date("Y-m-d")."') group by userid, tanggalPenimbangan");
+                }
+            }
+            
+            foreach ($query->result() as $datas){
+                if(array_key_exists($datas->userid, $users)){
+                    $data_count                  = $result_data[$users[$datas->userid]];
+                    if(array_key_exists($datas->submissiondate, $data_count)){
+                        $data_count[$datas->submissiondate] +=$datas->counts;
+                    }
+                    $result_data[$users[$datas->userid]] = $data_count;
+                }
+                
+            }
+        }
+        
+        return $result_data;
+    }
+    
     public function getCountPerMode($kecamatan="",$mode="Mingguan"){
         date_default_timezone_set("Asia/Makassar"); 
         $giziDB = $this->load->database('gizi', TRUE);
@@ -292,6 +375,66 @@ class GiziModel extends CI_Model{
         return $result_data;
     }
     
+    public function getCountPerFormByVisitDate($kecamatan="",$start,$end){
+        $end = date("Y-m-d",  strtotime($end." +1 day"));
+        $giziDB = $this->load->database('gizi', TRUE);
+        $query  = $giziDB->query("SHOW TABLES FROM opensrp_gizi");
+        $table_default = [
+            'kunjungan_gizi'=>'Kunjungan Gizi'];
+        //retrieve the tables name
+        $tables = array();
+        foreach ($query->result() as $table){
+            if(array_key_exists($table->Tables_in_opensrp_gizi, $table_default)){
+                $tables[$table->Tables_in_opensrp_gizi]=$table_default[$table->Tables_in_opensrp_gizi];
+            }
+        }
+        
+        if($kecamatan=='Sengkol'){
+            $users = ['gizi8'=>'Ketara','gizi9'=>'Sengkol','gizi10'=>'Sengkol','gizi11'=>'Kawo','gizi12'=>'Tanak Awu','gizi13'=>'Pengembur','gizi14'=>'Segala Anyar'];
+        }elseif($kecamatan=='Janapria'){
+            $users = ['gizi1'=>'Lekor','gizi2'=>'Saba','gizi3'=>'Pendem','gizi4'=>'Setuta','gizi5'=>'Jango','gizi6'=>'Janapria'];
+        }else{
+            return;
+        }
+        
+        //make result array from the tables name
+        $result_data = array();
+        foreach ($users as $user=>$desa){
+            $data = array();
+            foreach ($tables as $table=>$legend){
+                $table_name = 0;
+                $data[$legend] = $table_name;
+            }
+            $result_data[$desa] = $data;
+        }
+        
+        //retrieve all the columns in the table
+        $columns = array();
+        foreach ($table_default as $table=>$legend){
+            $query = $giziDB->query("SHOW COLUMNS FROM ".$table);
+            foreach ($query->result() as $column){
+                array_push($columns, $column->Field);
+            }
+            
+            //query tha data
+            if($kecamatan=='Sengkol'){
+                $query = $giziDB->query("SELECT userid, tanggalPenimbangan as submissiondate,count(*) as counts from ".$table." where (userid='gizi8' or userid='gizi9' or userid='gizi10' or userid='gizi11' or userid='gizi12' or userid='gizi13' or userid='gizi14') AND tanggalPenimbangan > '$start' AND tanggalPenimbangan <= '$end'  group by userid, tanggalPenimbangan");
+            }
+            elseif($kecamatan=='Janapria'){
+                $query = $giziDB->query("SELECT userid, tanggalPenimbangan as submissiondate,count(*) as counts from ".$table." where (userid='gizi1' or userid='gizi2' or userid='gizi3' or userid='gizi4' or userid='gizi5' or userid='gizi6') AND tanggalPenimbangan > '$start' AND tanggalPenimbangan <= '$end'  group by userid, tanggalPenimbangan");
+            }
+            foreach ($query->result() as $datas){
+                if(array_key_exists($datas->userid, $users)){
+                    $data_count                  = $result_data[$users[$datas->userid]];
+                    $data_count[$legend]         += $datas->counts;
+                    $result_data[$users[$datas->userid]] = $data_count;
+                }
+            }
+        }
+        
+        return $result_data;
+    }
+    
     public function getCountPerFormForDrill($desa="",$date=""){
         $giziDB = $this->load->database('gizi', TRUE);
         $query  = $giziDB->query("SHOW TABLES FROM opensrp_gizi");
@@ -364,6 +507,82 @@ class GiziModel extends CI_Model{
             //query tha data
             reset($users);
             $query3 = $giziDB->query("SELECT userid, DATE(clientversionsubmissiondate) as submissiondate,count(*) as counts from ".$table." where (userid='".key($users)."') and DATE(clientversionsubmissiondate)='".$date."' group by userid, DATE(clientversionsubmissiondate)");
+            foreach ($query3->result() as $datas){
+                if(array_key_exists($datas->userid, $users)){
+                    $data_count                  = $result_data[$date];
+                    if(array_key_exists($table, $table_default)){
+                        $data_count["data"][$tabindex[$table]][1]         += $datas->counts;
+                    }
+                    
+                    $result_data[$date] = $data_count;
+                }
+            }
+        }
+        
+        return $result_data;
+    }
+    
+    public function getCountPerFormByVisitDateForDrill($desa="",$date=""){
+        $giziDB = $this->load->database('gizi', TRUE);
+        $query  = $giziDB->query("SHOW TABLES FROM opensrp_gizi");
+        $table_default = [
+            'kunjungan_gizi'=>'Kunjungan Gizi'];
+        $tabindex = [
+            'kunjungan_gizi'=>0];
+        //retrieve the tables name
+        $tables = array();
+        foreach ($query->result() as $table){
+            if(array_key_exists($table->Tables_in_opensrp_gizi, $table_default)){
+                $tables[$table->Tables_in_opensrp_gizi]=$table_default[$table->Tables_in_opensrp_gizi];
+            }
+        }
+        
+        if($desa=="Lekor"){
+            $users = ['gizi1'=>'Lekor'];
+        }elseif($desa=="Saba"){
+            $users = ['gizi2'=>'Saba'];
+        }elseif($desa=="Pendem"){
+            $users = ['gizi3'=>'Pendem'];
+        }elseif($desa=="Setuta"){
+            $users = ['gizi4'=>'Setuta'];
+        }elseif($desa=="Jango"){
+            $users = ['gizi5'=>'Jango'];
+        }elseif($desa=="Janapria"){
+            $users = ['gizi6'=>'Janapria'];
+        }elseif($desa=="Ketara"){
+            $users = ['gizi8'=>'Ketara'];
+        }elseif($desa=="Sengkol"){
+            $users = ['gizi9'=>'Sengkol','gizi10'=>'Sengkol'];
+        }elseif($desa=="Kawo"){
+            $users = ['gizi11'=>'Kawo'];
+        }elseif($desa=="Tanak_Awu"){
+            $users = ['gizi12'=>'Tanak Awu'];
+        }elseif($desa=="Pengembur"){
+            $users = ['gizi13'=>'Pengembur'];
+        }elseif($desa=="Segala_Anyar"){
+            $users = ['gizi14'=>'Segala Anyar'];
+        }
+        
+        //make result array from the tables name
+        $result_data = array();
+        foreach ($users as $user=>$desa){
+            $data = array();
+            $data[$date] = array();
+            foreach ($table_default as $table=>$table_name){
+                $data[$date]["name"] = $date;
+                $data[$date]["id"] = $date;
+                $data[$date]["data"] = array();
+                foreach ($table_default as $td=>$td_name){
+                    array_push($data[$date]["data"], array($td_name,0));
+                }
+            }
+            $result_data = $data;
+        }
+        
+        foreach ($tables as $table=>$legend){
+            //query tha data
+            reset($users);
+            $query3 = $giziDB->query("SELECT userid, tanggalPenimbangan as submissiondate,count(*) as counts from ".$table." where (userid='".key($users)."') and tanggalPenimbangan='".$date."' group by userid, tanggalPenimbangan");
             foreach ($query3->result() as $datas){
                 if(array_key_exists($datas->userid, $users)){
                     $data_count                  = $result_data[$date];
