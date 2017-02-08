326481b7904dfef569f8e0f9e0a44804460820e8
AinulHamdani <hamdani.ainul@gmail.com>
2017-02-08 21:40:45 +0800

    AH | add get desa function

--- application/models/LocationModel.php
+++ application/models/LocationModel.php
@@ -10,13 +10,17 @@ class LocationModel extends CI_Model{
     
     private $loc = ["Darek"=>array('user1'=>'Pandan Indah','user2'=>'Serage','user3'=>'Teduh'),
                     "Pengadang"=>array('user1'=>'Gerantung','user2'=>'Jurang Jaler','user3'=>'Pengadang'),
-                    "Kopang"=>array('user1'=>'Aik Bual','user2'=>'Kopang Rembiga','user3'=>'Montong Gamang'),
+                    "Kopang"=>array('user21'=>'Aik Bual','user2'=>'Kopang Rembiga','user3'=>'Montong Gamang'),
                     "Mantang"=>array('user1'=>'Barabali','user2'=>'Mantang','user3'=>'Presak','user4'=>'Tampak Siring'),
                     "Mujur  "=>array('user1'=>'Mujur','user2'=>'Sukaraja'),
                     "Puyung"=>array('user1'=>'Dasan Ketujur','user2'=>'Gemel'),
-                    "Ubung"=>array('user1'=>'Batu Tulis','user2'=>'Labulia','user3'=>'Ubung')];
+                    "Ubung"=>array('user20'=>'Batu Tulis','user2'=>'Labulia','user3'=>'Ubung')];
     
     public function getAllLoc(){
         return $this->loc;
+    }
+    
+    public function getDesa($kec){
+        return $this->loc[$kec];
     }
 }
