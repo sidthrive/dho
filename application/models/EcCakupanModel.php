c02ef8206718d58eb227f8dafd5ab16f68196544
AinulHamdani <hamdani.ainul@gmail.com>
2017-02-21 20:23:54 +0800

    AH | fix container

--- application/models/EcCakupanModel.php
+++ application/models/EcCakupanModel.php
@@ -11,13 +11,14 @@ class EcCakupanModel extends CI_Model{
     
     public function getCakupanContainer($fhw,$p=FALSE){
         $loc = $this->loc->getIntLocId($fhw);
+        $ret = [];
         foreach ($loc as $loc1=>$loc2){
             if($fhw=='vaksinator'){
                 if($p){
-                    $loc[$loc1] = array('lbl'=>0,'pbl'=>0,'lbi'=>0,'pbi'=>0);
-                }else $loc[$loc1] = array('l'=>0,'p'=>0);
-            }else $loc[$loc1] = 0;
+                    $ret[$loc2] = array('lbl'=>0,'pbl'=>0,'lbi'=>0,'pbi'=>0);
+                }else $ret[$loc2] = array('l'=>0,'p'=>0);
+            }else $ret[$loc2] = 0;
         }
-        return $loc;
+        return $ret;
     }
 }
