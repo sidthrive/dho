7ebdbfc785fdeb0d4c7761011566615eaadd2143
AinulHamdani <hamdani.ainul@gmail.com>
2017-04-04 11:32:05 +0800

    AH | update header text

--- application/views/header.php
+++ application/views/header.php
@@ -32,13 +32,13 @@ additional bootstrap import for sidebar collapse style
                 <p style="text-align: right;">| <?php echo $_SESSION['username'];?> | <a href="<?= site_url('welcome/logout') ?>" rel="nofollow">Logout</a> |</p>
             </div>
             <div id="logo">
-                <img src="<?php echo base_url() ?>assets/images/logo_loteng.gif" alt="" align="left" style="padding-right: 10px;"/>
+                <img src="<?php echo base_url() ?>assets/images/ntb.png" alt="" align="left" style="padding-right: 10px;"/>
 <!--                <img src="<?php echo base_url() ?>assets/images/sid.jpg" alt="" align="left" style="padding-right: 10px;"/>
                 <img src="<?php echo base_url() ?>assets/images/opensrp.png" alt="" align="left" style="padding-right: 10px;"/>-->
             </div>
             <div id="head" class="center">
-                <h1>DHO Report</h1>
-                <h3 style="margin-top: 5px;">Dinas Kesehatan Lombok Tengah</h3>
+                <h1>PHO Report</h1>
+                <h3 style="margin-top: 5px;">Dinas Kesehatan Provinsi NTB</h3>
 <!--                <h3 style="margin-top: 5px;">Summit Institute of Development</h3>-->
             </div>
         </div>
