5b76a6e8adca749ecede41de32fff2db805529ef
AinulHamdani <hamdani.ainul@gmail.com>
2017-04-04 10:43:13 +0800

    AH | add pws database

--- application/config/database.php
+++ application/config/database.php
@@ -116,3 +116,25 @@ $db['analytics'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['pws'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'dho_pws',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
