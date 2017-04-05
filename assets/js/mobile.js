c33dab4c9c4e5e68bb81d4efccfc168beca1fa94
AinulHamdani <hamdani.ainul@gmail.com>
2017-04-05 17:23:04 +0800

    AH | update css and js

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -3,6 +3,7 @@ $(function(){
     $('#slide-submenu').on('click',function() {			        
         $('#sidebar1').toggle('slide',function(){
                 $('#mini-submenu').fadeIn();	
+                $("#content").css({"width":"100%"});
         });
 
       });
@@ -10,5 +11,6 @@ $(function(){
     $('#mini-submenu').on('click',function(){		
         $('#sidebar1').toggle('slide');
         $('#mini-submenu').hide();
+        $("#content").css({"width":"700px"});
         })
 })
--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -9,20 +9,14 @@ $(window).on("scroll",function (e){
     function WidthChange(mq) {
       if (mq.matches) {
         if ($(this).scrollTop() >= 265) {
-            $("#sidebar1").css({"position":"fixed","top":"80px","z-index":"10000"});
             $("#menu").css({"position":"fixed","top":"0px","z-index":"100000","width":"100%"});
-            $("#content").css({"margin-left":"300px"});
         } else {
-            $("#sidebar1").css({"position":"","top":"","z-index":""});
             $("#menu").css({"position":"","top":"","z-index":"","width":""});
-            $("#content").css({"margin-left":""});
         }
       } else {
         if ($(this).scrollTop() >= 150) {
-            $("#sidebar1").css({"position":"fixed","top":"100px","z-index":"10000"});
             $("#menu").css({"position":"fixed","top":"0px","z-index":"100000","width":"100%"});
         } else {
-            $("#sidebar1").css({"position":"","top":"","z-index":""});
             $("#menu").css({"position":"","top":"","z-index":"","width":""});
         }
       }
