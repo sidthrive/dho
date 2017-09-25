8daddd7e8ee71ea66724571fa5e4056e24ac193c
AinulHamdani <hamdani.ainul@gmail.com>
2017-09-25 14:22:26 +0800

    AH | add new handscore to ec

--- application/views/hhhscore/hhhsidebar.php
+++ application/views/hhhscore/hhhsidebar.php
@@ -18,7 +18,7 @@
                     </h4>
                 </div>
                 </a>
-                <div id="bidan_menu" class="panel-collapse collapse<?=($this->uri->segment(2)=='headscore'||(strpos($this->uri->segment(2),'trimester')!==false)||$this->uri->segment(2)=='heartscore'||$this->uri->segment(2)=='standar')?' in':''?>">
+                <div id="bidan_menu" class="panel-collapse collapse<?=($this->uri->segment(2)=='headscore'||(strpos($this->uri->segment(2),'trimester')!==false)||$this->uri->segment(2)=='heartscore'||$this->uri->segment(2)=='handscore')?' in':''?>">
                     <div class="panel panel-default">
                         <a data-parent="#bidan_menu" href="<?php echo site_url() ."hhhscore/headscore"?>">
                         <div <?=($this->uri->segment(2)=='headscore')?' style="background-color:#909090"':''?> class="panel-heading">
@@ -29,58 +29,13 @@
                         </a>
                     </div>
                     <div class="panel panel-default">
-                        <a data-toggle="collapse" data-parent="#bidans" href="#handscore">
-                        <div class="panel-heading">
+                        <a data-parent="#bidan_menu" href="<?php echo site_url() ."hhhscore/handscore"?>">
+                        <div <?=($this->uri->segment(2)=='handscore')?' style="background-color:#909090"':''?> class="panel-heading">
                             <h4 class="panel-title">
                                 Hand Score
                             </h4>
                         </div>
                         </a>
-                        <div id="handscore" class="panel-collapse collapse<?=(strpos($this->uri->segment(2),'trimester')!==false||$this->uri->segment(2)=='standar')?' in':''?>">
-                            <div class="panel panel-default">
-                                <a data-toggle="collapse" data-parent="#handscore" href="#qci">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title">
-                                        Pelayanan Antenatal (QCI)
-                                    </h4>
-                                </div>
-                                </a>
-                                <div id="qci" class="panel-collapse collapse<?=(strpos($this->uri->segment(2),'trimester')!==false)?' in':''?>">
-                                    <div class="panel panel-default">
-                                        <a data-parent="#qci" href="<?php echo site_url() ."hhhscore/bidantrimester1"?>">
-                                        <div class="panel-heading"<?=($this->uri->segment(2)=='bidantrimester1')?' style="background-color:#909090"':''?>>
-                                            <h4 class="panel-title">
-                                                Trimester 1
-                                            </h4>
-                                        </div>
-                                        </a>
-                                        <a data-parent="#qci" href="<?php echo site_url() ."hhhscore/bidantrimester2"?>">
-                                        <div class="panel-heading"<?=($this->uri->segment(2)=='bidantrimester2')?' style="background-color:#909090"':''?>>
-                                            <h4 class="panel-title">
-                                                Trimester 2
-                                            </h4>
-                                        </div>
-                                        </a>
-                                        <a data-parent="#qci" href="<?php echo site_url() ."hhhscore/bidantrimester3"?>">
-                                        <div class="panel-heading"<?=($this->uri->segment(2)=='bidantrimester3')?' style="background-color:#909090"':''?>>
-                                            <h4 class="panel-title">
-                                                Trimester 3
-                                            </h4>
-                                        </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="panel panel-default">
-                                <a data-parent="#bidan_menu" href="<?php echo site_url() ."hhhscore/standar"?>">
-                                <div class="panel-heading"<?=($this->uri->segment(2)=='standar')?' style="background-color:#909090"':''?>>
-                                    <h4 class="panel-title">
-                                        Cakupan Pelayanan
-                                    </h4>
-                                </div>
-                                </a>
-                            </div>
-                        </div>
                     </div>
                     <div class="panel panel-default">
                         <a data-parent="#bidan_menu" href="<?php echo site_url() ."hhhscore/heartscore"?>">
@@ -92,28 +47,6 @@
                         </a>
                     </div>
                 </div>
-            </div>
-            <?php } ?>
-            <?php if($this->session->userdata('tipe')=="gizi"||$this->session->userdata('tipe')=="all"){ ?>
-            <div class="panel panel-default">
-                <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        Gizi
-                    </h4>
-                </div>
-                </a>
-            </div>
-            <?php } ?>
-            <?php if($this->session->userdata('tipe')=="vaksinator"||$this->session->userdata('tipe')=="all"){ ?>
-            <div class="panel panel-default">
-                <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        Vaksinator
-                    </h4>
-                </div>
-                </a>
             </div>
             <?php } ?>
         </div>
